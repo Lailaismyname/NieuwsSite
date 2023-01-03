98781e8b23294a2518f59f94a69d9c44d4244541
laila <91194082+Lailaismyname@users.github.com>
2023-01-03 17:29:57 +0100

    blind me with your light

---
+++ news.js
@@ -0,0 +1,8 @@
+const mobToggleBtnBelangrijk = document.querySelector('#mobToggleBtnBelangrijk');
+const mobToggleBtnLaatsteNieuws = document.querySelector('#mobToggleBtnLaatsteNieuws');
+
+function test() {
+    alert('test');
+} 
+
+mobToggleBtnLaatsteNieuws.addEventListener('click', test);
