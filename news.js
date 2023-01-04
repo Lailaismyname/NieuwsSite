74c8c8f83bc09aca68f3a3127ccacdfa427b70df
laila <91194082+Lailaismyname@users.github.com>
2023-01-04 16:50:48 +0100

    footer en notities voor later

--- news.js
+++ news.js
@@ -1,8 +1,8 @@
 const mobToggleBtnBelangrijk = document.querySelector('#mobToggleBtnBelangrijk');
 const mobToggleBtnLaatsteNieuws = document.querySelector('#mobToggleBtnLaatsteNieuws');
-
+const nieuwsbriefAanmeldBtn = document.querySelector('#nieuwsbriefAanmeldBtn');
 function test() {
     alert('test');
 } 
 
-mobToggleBtnLaatsteNieuws.addEventListener('click', test);
+nieuwsbriefAanmeldBtn.addEventListener('click', test);
