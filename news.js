8a9155ebc2a2e6daea439df11c209564344a43aa
laila <91194082+Lailaismyname@users.github.com>
2023-01-05 17:38:06 +0100

    content toegevoegd functie voor nieuws content toggle toegevoegd css aangepast

--- news.js
+++ news.js
@@ -1,8 +1,25 @@
-const mobToggleBtnBelangrijk = document.querySelector('#mobToggleBtnBelangrijk');
-const mobToggleBtnLaatsteNieuws = document.querySelector('#mobToggleBtnLaatsteNieuws');
+const mobToggleBtnBelangrijkBtn = document.querySelector('#mobToggleBtnBelangrijk');
+const mobToggleBtnLaatsteNieuwsBtn = document.querySelector('#mobToggleBtnLaatsteNieuws');
+const toggleChoiceBox = document.querySelector('#mob-toggle-btn-choice');
 const nieuwsbriefAanmeldBtn = document.querySelector('#nieuwsbriefAanmeldBtn');
-function test() {
-    alert('test');
-} 
+const laatsteNieuws = document.querySelector('#laatsteNieuws');     //laatste nieuws = rechter content
+const belangrijkNieuws = document.querySelector('#belangrijkNieuws');               //belangrijk nieuws = linker content
 
-nieuwsbriefAanmeldBtn.addEventListener('click', test);
+//toggle tussen belangrijk en laatste nieuws. Alleen bij kleine schermen.
+toggleChoiceBox.addEventListener('click', function (e){
+    //als breedte scherm kleiner dan tablet (768px)
+    if(window.screen.width < 767){
+        if(e.target.id == 'mobToggleBtnBelangrijk'){
+            if(belangrijkNieuws.classList.contains('hide-element')){
+                belangrijkNieuws.classList.remove('hide-element');
+                laatsteNieuws.classList.add('hide-element');
+            }
+        }
+        else if(e.target.id == 'mobToggleBtnLaatsteNieuws'){
+            if(laatsteNieuws.classList.contains('hide-element')){
+                laatsteNieuws.classList.remove('hide-element');
+                belangrijkNieuws.classList.add('hide-element');
+            }
+        }
+    }
+});
